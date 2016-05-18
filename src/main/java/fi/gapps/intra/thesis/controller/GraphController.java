634227ab82146d3b65a79393e8c939dd7903907f
roxyroxy <roxana.roman@gapps.fi>
2016-05-18 13:01:02 +0300

    basic security

--- src/main/java/fi/gapps/intra/thesis/controller/GraphController.java
+++ src/main/java/fi/gapps/intra/thesis/controller/GraphController.java
@@ -1,6 +1,6 @@
 package fi.gapps.intra.thesis.controller;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fi.gapps.intra.thesis.model.Edge;
@@ -24,71 +22,82 @@ public class GraphController {
 
 	@Autowired
 	private VertexService vertexService;
-	
+
 	@Autowired
 	private EdgeService edgeService;
+
+	private static final String TOKEN ="fb080452-1cc9-11e6-b6ba-3e1d05defe78";
+	private static final String AUTH_MSG = "ERROR: NOT AUTHENTICATED!";
 	@Transactional
 	@RequestMapping(value = "vertex/all", method = RequestMethod.POST, consumes = "application/json")
 	public void addVertex(@RequestBody RequestObject reqObject) {
 		String token = reqObject.getToken();
-		if(!token.equals("fb080452-1cc9-11e6-b6ba-3e1d05defe78")){
-			System.out.println("ERROR: NOT AUTHENTICATED!!!!");
-			return;	
+		if (!token.equals(TOKEN)) {
+			System.out.println(AUTH_MSG);
+			return;
 		}
 		List<Vertex> vertices = reqObject.getVertices();
 		for (Vertex v : vertices) {
 			Vertex old = vertexService.findByEmail(v.getEmail());
 			if (old == null) {
 				vertexService.create(v);
-			}else{
-				for(Edge e: v.getTeammates()){
+			} else {
+				for (Edge e : v.getTeammates()) {
 					boolean found = false;
-					for(Edge t: old.getTeammates()){
-						if (e.equals(t)){
+					for (Edge t : old.getTeammates()) {
+						if (e.equals(t)) {
 							found = true;
 						}
 					}
-					if(found == false)
-					old.worksWith(e);
+					if (found == false)
+						old.worksWith(e);
 				}
-			//	vertexService.create(old);
 			}
 		}
-	
+
 		System.out.println("Done");
 
 	}
-	
-	
+
 	@Transactional(readOnly = true)
 	@RequestMapping(value = "vertex", method = RequestMethod.GET)
 	public Iterable<Vertex> getAllVertices() {
 		return vertexService.findAll();
 	}
-	
+
 	@Transactional(readOnly = true)
 	@RequestMapping(value = "vertex/teammates", method = RequestMethod.GET)
-	public List<String> getCommunity(@RequestHeader (name="email") String email) {
+	public List<String> getCommunity(@RequestHeader(name = "email") String email,
+			@RequestHeader(name = "token") String token) {
+		if(!token.equals(TOKEN)){
+			System.out.println(AUTH_MSG);
+			return new ArrayList<>();
+		}
 		System.out.println("Email " + email);
 		return vertexService.getCommunity(email);
 	}
-	
+
 	@Transactional(readOnly = true)
 	@RequestMapping(value = "vertex/topFriends", method = RequestMethod.GET)
-	public List<String> getTopFriends(@RequestHeader (name="email") String email) {
+	public List<String> getTopFriends(@RequestHeader(name = "email") String email,
+			@RequestHeader(name = "token") String token) {
+		if(!token.equals(TOKEN)){
+			System.out.println(AUTH_MSG);
+			return new ArrayList<>();
+		}
 		System.out.println("Email " + email);
 		return vertexService.getTopThree(email);
 	}
-	
-	@Transactional
+
+	@Transactional(readOnly = true)
 	@RequestMapping(value = "vertex/delete", method = RequestMethod.DELETE)
 	public void deleteAll() {
-		Iterable<Vertex> vertices  = vertexService.findAll();
-		for(Vertex v: vertices ){
+		Iterable<Vertex> vertices = vertexService.findAll();
+		for (Vertex v : vertices) {
 			vertexService.delete(v);
 		}
 		Iterable<Edge> edges = edgeService.findAll();
-		for(Edge e: edges){
+		for (Edge e : edges) {
 			edgeService.delete(e);
 		}
 
