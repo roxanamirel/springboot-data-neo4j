4e3379531596ae83fc35e3de90cc08f02a9f767b
roxyroxy <roxana.roman@gapps.fi>
2016-06-02 15:22:16 +0300

    changed to basic saving

--- src/main/java/fi/gapps/intra/thesis/controller/GraphController.java
+++ src/main/java/fi/gapps/intra/thesis/controller/GraphController.java
@@ -37,72 +37,21 @@ public class GraphController {
 			System.out.println(AUTH_MSG);
 			return;
 		}
-
-		// Vertex v = new Vertex();
-		// v.setEmail("ysyysys@gmail.com");
-		// v.setName("ICSY");
-		// v.setUniverseUserId(3l);
-		// for(int i=0;i<40;i++){
-		// Vertex vv = new Vertex();
-		// vv.setEmail("email" + i + "@yahoo.com");
-		// vv.setName("NAme" + i);
-		// vv.setUniverseUserId(Long.valueOf(i));
-		// Edge edge = new Edge();
-		// edge.setDest(vv);
-		// edge.setSrc(v);
-		// edge.setWeight(i);
-		// v.worksWith(edge);
-		// }
-		// vertexService.create(v);
-
 		List<Vertex> vertices = reqObject.getVertices();
 		for (Vertex v : vertices) {
 			Vertex old = vertexService.findByEmail(v.getEmail());
 			if (old == null) {
-
-				System.out.println("Teammates of " + v.getEmail());
-				Vertex newV = new Vertex();
-				newV.setEmail(v.getEmail());
-				newV.setName(v.getName());
-				newV.setUniverseUserId(v.getUniverseUserId());
-				for (Edge e : v.getTeammates()) {
-					Edge newE = new Edge();
-					Vertex dest = new Vertex();
-					dest.setEmail(e.getDest().getEmail());
-					dest.setName(e.getDest().getName());
-					dest.setUniverseUserId(e.getDest().getUniverseUserId());
-					if (e.getDest() != null) {
-						for (Edge dd : e.getDest().getTeammates()) {
-							dest.worksWith(dd);
-						}
-					}
-
-					newE.setDest(dest);
-					newE.setSrc(newV);
-					newE.setWeight(e.getWeight());
-					newV.worksWith(newE);
-				}
-				vertexService.create(newV);
+				vertexService.create(v);
 			} else {
-				System.out.println("Adding teammates of " + v.getEmail());
-				if (v.getTeammates() != null) {
-					for (Edge e : v.getTeammates()) {
-						boolean found = false;
-						if (old.getTeammates() != null) {
-							for (Edge t : old.getTeammates()) {
-								if (e.getDest().getEmail().equals(t.getDest().getEmail())) {
-									found = true;
-									System.out.println("Already a teammate of " + e.getDest().getEmail());
-								}
-							}
-							if (found == false) {
-								System.out.println("Adding teammate " + e.getDest().getEmail());
-								old.worksWith(e);
-							}
+				for (Edge e : v.getTeammates()) {
+					boolean found = false;
+					for (Edge t : old.getTeammates()) {
+						if (e.equals(t)) {
+							found = true;
 						}
 					}
-				} else {
-					System.out.println("no teamamtes");
+					if (found == false)
+						old.worksWith(e);
 				}
 			}
 		}
