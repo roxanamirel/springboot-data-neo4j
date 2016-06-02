932cd9b858e7e179256ec9674d57e5500a97d30e
roxyroxy <roxana.roman@gapps.fi>
2016-06-02 11:03:31 +0300

    test3

--- src/main/java/fi/gapps/intra/thesis/controller/GraphController.java
+++ src/main/java/fi/gapps/intra/thesis/controller/GraphController.java
@@ -40,7 +40,7 @@ public class GraphController {
 
 		List<Vertex> vertices = reqObject.getVertices();
 		for (Vertex v : vertices) {
-			System.out.println(v.toString());
+			System.out.println(v.getEmail() + ";" + v.getName() + ";" + v.getTeammates().size());
 		}
 
 		// Vertex v = new Vertex();
